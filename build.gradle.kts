fcc5fc71ec3e518c2144cadfd73b1738e4e9abc5
Santos Zatarain Vera <santoszv@inftel.com.mx>
2022-02-10 23:24:17 -0600

    2022.02.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ version = "2022.02.10"
 
 plugins {
     `java-library`
+    `maven-publish`
+    signing
 }
 
 repositories {
@@ -16,8 +18,62 @@ dependencies {
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+    withSourcesJar()
+    withJavadocJar()
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Javadoc>().configureEach {
+    options.encoding = "UTF-8"
 }
 
 tasks.jar.configure {
     archiveBaseName.set("sat-cfd")
+}
+
+publishing {
+    repositories {
+        maven {
+            setUrl(file("$projectDir/build/repo"))
+        }
+    }
+
+    publications {
+        create<MavenPublication>("cfd") {
+            from(components["java"])
+            pom {
+                name.set("SAT CFD")
+                description.set("Java classes and resources for SAT's CFD. Clases Java y archivos para CFD del SAT.")
+                url.set("https://github.com/santoszv/sat-cfd")
+                inceptionYear.set("2022")
+                licenses {
+                    license {
+                        name.set("Public domain")
+                        url.set("https://en.wikipedia.org/wiki/Public_domain")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("santoszv")
+                        name.set("Santos Zatarain Vera")
+                        email.set("santoszv@inftel.com.mx")
+                        url.set("https://www.inftel.com.mx")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/santoszv/sat-cfd")
+                    developerConnection.set("scm:git:https://github.com/santoszv/sat-cfd")
+                    url.set("https://github.com/santoszv/sat-cfd")
+                }
+            }
+            signing.sign(this)
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
 }
